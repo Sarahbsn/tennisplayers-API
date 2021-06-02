d134283cfc62688951b517e6bf52e86cae174b92
Sarah Bensouna <sarah.bensouna@hotmail.com>
2021-06-03 00:12:37 +0200

    get request by player id created

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-	res.send("ca marche");
+	res.send("I love tennis");
 });
 
 module.exports = router;
--- routes/players.js
+++ routes/players.js
@@ -12,4 +12,20 @@ router.get("/", (req, res) => {
 	});
 });
 
+router.get("/:id", (req, res) => {
+	const playerId = req.params.id;
+	connection.query(
+		"SELECT * FROM tennis_players WHERE id = ?",
+		[playerId],
+		(err, results) => {
+			if (err) {
+				res.status(500).send("Error retrieving players from database");
+			} else {
+				if (results.length) res.status(200).json(results[0]);
+				else res.status(404).send("player not found");
+			}
+		}
+	);
+});
+
 module.exports = router;
